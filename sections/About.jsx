51d2e11864d4ca1c22a856bd08dc2b0e52ebeb24
Sebastian Mera <107935758+Samera1@users.noreply.github.com>
2023-03-19 23:38:37 -0500

    Traduccion-Actualizacion

--- sections/About.jsx
+++ sections/About.jsx
@@ -17,16 +17,19 @@ const About = () => (
     >
       <TypingText title="| Sobre el Metaverso" textStyles="text-center" />
 
-      <motion.p 
-        variants={fadeIn('up', 'tween', 0.2, 1 )}
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
         className=" mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        Metaverso
-        
-        es algo nuevo en el futuro, donde puedes disfrutar del mundo virtual sintiendo que es realmente real, puedes sentir lo que sientes en este mundo de metaverso, porque esta es realmente la locura del metaverso de hoy, usando solo dispositivos VR puede explorar fácilmente el mundo del metaverso que desea, convertir sus sueños en realidad. Exploremos la locura del metaverso desplazándonos hacia abajo
+        <span className="font-extrabold text-white">Metaverso</span> es algo nuevo en el futuro, donde puedes disfrutar del mundo virtual sintiendo que es realmente real, puedes sentir lo que sientes en este mundo de metaverso, porque esta es realmente la <span className="font-extrabold text-white">locura del metaverso</span> de hoy, usando solo dispositivos <span className="font-extrabold text-white">VR</span> puede explorar fácilmente el mundo del metaverso que desea, convertir sus sueños en realidad. <span className="font-extrabold text-white">Exploremos</span> la locura del metaverso desplazándonos hacia abajo
       </motion.p>
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow-down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
     </motion.div>
-    About section
   </section>
 );
 
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -7,22 +7,39 @@ import { TypingText, ExploreCard, TitleText } from '../components';
 import { staggerContainer } from '../utils/motion';
 import { exploreWorlds } from '../constants';
 
-const Explore = () => (
-  <section className={`${styles.paddings}`}
-    id="explore"
-  >
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
+  return (
+    <section className={`${styles.paddings}`}
+      id="explore"
     >
-      <TypingText title="| El Mundo"
-      textStyles="text-center"/>
-    </motion.div>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| El Mundo"
+          textStyles="text-center"
+        />
+        <TitleText title={<> Elige el mundo que quieras <br className="md:block hidden" /> ir a explorar  </>} textStyles="text-center" />
 
-  </section>
-);
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+
+    </section>
+  );
+};
 
 export default Explore;
